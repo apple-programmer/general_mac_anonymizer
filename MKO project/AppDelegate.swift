b46e3befbc536ee19c39281031426ef7fc6889ac
Roman Nikitin <NikRom@macbook-air-roman.local>
2016-02-06 17:28:21 +0300

    Initial Commit

---
+++ MKO project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MKO project
+//
+//  Created by Roman Nikitin on 06.02.16.
+//  Copyright © 2016 NikRom. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MKO project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MKO project
+//
+//  Created by Roman Nikitin on 06.02.16.
+//  Copyright © 2016 NikRom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
